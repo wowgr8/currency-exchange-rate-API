8269ad141eb813311b0112c611b4da28a661aad2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-13 16:08:02 -0800

    add typo fix to styles.css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.cc';
+import './css/styles.css';
 import ExchangeService from './js/exchange-service.js'
 
 function clearFields() {
