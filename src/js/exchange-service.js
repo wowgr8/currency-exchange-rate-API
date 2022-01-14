b1e87e924d7edd15abe8d363b9f0376b59670a95
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-13 17:49:15 -0800

    add rough draft url

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -2,7 +2,7 @@ export default class ExchangeService {
   static exchangeRate(currency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = ``;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromCurrency}/${toCurrency}/${amount}`; //usd and eur and 30 will need place holders. originally: https://v6.exchangerate-api.com/v6/ee953c2d929db82509d7d3ba/pair/USD/EUR/30
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
