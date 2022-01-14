03f243bce467662a68adab1c5d97e88a40a87074
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-13 16:03:19 -0800

    add api request draft without url

---
+++ src/js/exchange-service.js
@@ -0,0 +1,17 @@
+export default class ExchangeService {
+  static exchangeRate(currency) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = ``;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
