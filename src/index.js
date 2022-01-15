df11e5ae924e359d719c58f97ad6b9316c201670
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-15 14:46:55 -0800

    add commas to imports and event.preventDefault to .click function, also updated variable values to match html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ExchangeService from './js/exchange-service.js'
+import ExchangeService from './js/exchange-service.js';
 
 function clearFields() {
   $('#amountUSD').val("");
@@ -12,13 +12,18 @@ function clearFields() {
 
 $(document).ready(function() {
   $('#exchangeBtn').click(function() {
-    let baseCurrency = $('#fromCurrency').val();
-    let targetCurrency = $('#toCurrency').val();
+    event.preventDefault();
+    console.log("test1");
+    let fromCurrency = $('#fromCurrency').val();
+    let toCurrency = $('#toCurrency').val();
+    let amount = $('#amountToExchange').val();
+    console.log("test");
     clearFields();
-    let promise = ExchangeService.exchangeRate(currency);
+    let promise = ExchangeService.exchangeRate(fromCurrency, toCurrency, amount);
     promise.then(function(response) {
+      console.log("hello");
       const body = JSON.parse(response);
-      $('.showExchangeRate').text(`Your new amount in ${targetCurrency} is ${body.conversion_result}`)
+      $('.showExchangeRate').text(`Your new amount in ${toCurrency} is ${body.conversion_result}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
