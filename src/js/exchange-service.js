4f8871dcb40bac996f28ca2682e0f6d92eba02cf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-01-16 00:12:03 -0800

    add parameters to static exchangeRate as well as semicolons

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,15 +1,15 @@
 export default class ExchangeService {
-  static exchangeRate(currency) {
+  static exchangeRate(fromCurrency, toCurrency, amount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromCurrency}/${toCurrency}/${amount}`; //usd and eur and 30 will need place holders. originally: https://v6.exchangerate-api.com/v6/ee953c2d929db82509d7d3ba/pair/USD/EUR/30
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromCurrency}/${toCurrency}/${amount}`; //Originally: https://v6.exchangerate-api.com/v6/ee953c2d929db82509d7d3ba/pair/USD/EUR/30
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
